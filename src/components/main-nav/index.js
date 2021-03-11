652f169c094020fdacfb5eddb05b5bf227d25898
bdoney20 <bdoney20@gmail.com>
2021-03-11 14:59:54 -0700

    Responsive

--- src/components/main-nav/index.js
+++ src/components/main-nav/index.js
@@ -1,41 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classnames from 'classnames';
 import './index.css';
 
 const MainNav = () => {
+    const [open, setOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setOpen(!open)
+    }
+
     return (
-        <div className="main-nav">
-            <ul>
-                <div className="logo-container">
-                    <a className="ifit-logo" href="/">
-                        <img src='https://cdn.ifit.com/img/ifit-logo-dark.svg' alt='iFit'/>
-                        COACH
-                    </a>
-                </div>
-                <li>
-                    <a className="page-link" href="/">
-                        <span>Exercise</span>
-                    </a>
-                </li>
-                <li>
-                    <a className="page-link" href="/">
-                        <span>Nutrition</span>
-                    </a>
-                </li>
-                <li>
-                    <a className="page-link" href="/">
-                        <span>Activate</span>
-                    </a>
-                </li>
-                <li>
-                    <a className="page-link" href="/">
-                        <span>Sleep</span>
-                    </a>
-                </li>
-                <div className="button-container">
-                    <button className="sign-up-button">Sign Up</button>
-                </div>
-            </ul>   
-        </div>
+        <header>
+            <div className="logo-container">
+                <a className="ifit-logo" href="/">
+                     <img src='https://cdn.ifit.com/img/ifit-logo-dark.svg' alt='iFit'/>
+                    COACH
+                </a>
+            </div>
+            <button onClick={toggleMenu}>&#9776;</button>
+            <div className="main-nav">
+                <ul className={classnames("hide", {'mobile-menu': open,})}>
+                    <li><a className="page-link" href="/"><span>Exercise</span></a></li>
+                    <li><a className="page-link" href="/"><span>Nutrition</span></a></li>
+                    <li><a className="page-link" href="/"><span>Activate</span></a></li>
+                    <li><a className="page-link" href="/"><span>Sleep</span></a></li>
+                    <div className="button-container">
+                        <button className="sign-up-button">Sign Up</button>
+                    </div>
+                </ul> 
+            </div>
+        </header>
+        
     )
 }
 
--- src/components/media-slider/index.js
+++ src/components/media-slider/index.js
@@ -6,10 +6,9 @@ import mashable from '../../images/third-party-log/mashable_logo.jpg';
 import './index.scss';
 
 const MediaSlider = () => {
-    
+
     const sliderSettings = {
         infinite: true,
-        className: "media-slider",
         draggable: true,
         focusOnSelect: true,
         slidesToShow: 1,
@@ -20,6 +19,7 @@ const MediaSlider = () => {
     }
 
      return (
+         <div className="media-slider">
             <Slider {...sliderSettings}>
                 <div className="slide">
                     <img src={gearjunkie} alt="gear junkie"/>
@@ -33,7 +33,20 @@ const MediaSlider = () => {
                     <img src={mashable} alt="mashable"/>
                     <p>&quot;Breathes new life into a tired, old running routine.&quot;</p>
                 </div>
+                <div className="slide">
+                    <img src={gearjunkie} alt="gear junkie"/>
+                    <p>&quot;You focus on putting in the work, and the technology handles the rest.&quot;</p>
+                </div>
+                <div className="slide">
+                    <img src={wired} alt="wired"/>
+                    <p>&quot;Literally transports you from home to wherever you choose to run.&quot;</p>
+                </div>
+                <div className="slide">
+                    <img src={mashable} alt="mashable"/>
+                    <p>&quot;Breathes new life into a tired, old running routine.&quot;</p>
+                </div>
             </Slider>
+        </div>
      )
 }
 
